5afe65d16c7e77c0233a98da858d8e98abaccdc5
suriya <suriyag88@gmail.com>
2022-10-24 23:10:01 -0700

    dataset info update

--- src/lib/hf_datasets_info.py
+++ src/lib/hf_datasets_info.py
@@ -2,175 +2,215 @@
 
 from datasets import ClassLabel, Value
 
-valid_task_names=['boolq', 'cb', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc', 'wsc.fixed', 'axb', 'axg', 'cola', 'sst2', 'mrpc', 'qqp', 'stsb', 'mnli', 'mnli_mismatched', 'mnli_matched', 'qnli', 'wnli', 'ax', 'imdb', 'hans']
+valid_task_names=['boolq', 'cb', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc', 'wsc.fixed', 'axb', 'axg', 'cola', 'sst2', 'mrpc', 'qqp', 'stsb', 'mnli', 'mnli_mismatched', 'mnli_matched', 'qnli', 'wnli', 'ax', 'imdb', 'hans', 'squad', 'squad_v2']
 
 datasets_info = {
 
-	"boolq":{
-		"load_dataset_args":('super_glue', 'boolq'),
-		"sentence_keys":['question', 'passage'],
-		"label":ClassLabel(num_classes=2, names=('False', 'True'), id=None),
-		"splits":['test', 'train', 'validation'],
+	"boolq": {
+		"load_dataset_args": ('super_glue', 'boolq'),
+		"sentence_keys": ['question', 'passage'],
+		"label": ClassLabel(num_classes=2, names=('False', 'True'), id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [3245, 9427, 3270],
 	},
 
-	"cb":{
-		"load_dataset_args":('super_glue', 'cb'),
-		"sentence_keys":['premise', 'hypothesis'],
-		"label":ClassLabel(num_classes=3, names=['entailment', 'contradiction', 'neutral'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"cb": {
+		"load_dataset_args": ('super_glue', 'cb'),
+		"sentence_keys": ['premise', 'hypothesis'],
+		"label": ClassLabel(num_classes=3, names=['entailment', 'contradiction', 'neutral'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [250, 250, 56],
 	},
 
-	"copa":{
-		"load_dataset_args":('super_glue', 'copa'),
-		"sentence_keys":['premise', 'choice1', 'choice2', 'question'],
-		"label":ClassLabel(num_classes=2, names=['choice1', 'choice2'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"copa": {
+		"load_dataset_args": ('super_glue', 'copa'),
+		"sentence_keys": ['premise', 'choice1', 'choice2', 'question'],
+		"label": ClassLabel(num_classes=2, names=['choice1', 'choice2'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [500, 400, 100],
 	},
 
-	"multirc":{
-		"load_dataset_args":('super_glue', 'multirc'),
-		"sentence_keys":['paragraph', 'question', 'answer'],
-		"label":ClassLabel(num_classes=2, names=('False', 'True'), id=None),
-		"splits":['test', 'train', 'validation'],
+	"multirc": {
+		"load_dataset_args": ('super_glue', 'multirc'),
+		"sentence_keys": ['paragraph', 'question', 'answer'],
+		"label": ClassLabel(num_classes=2, names=('False', 'True'), id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [9693, 27243, 4848],
 	},
 
-	"record":{
-		"load_dataset_args":('super_glue', 'record'),
-		"sentence_keys":['passage', 'query', 'entities', 'entity_spans', 'answers'],
-		"label":None,
-		"splits":['train', 'validation', 'test'],
+	"record": {
+		"load_dataset_args": ('super_glue', 'record'),
+		"sentence_keys": ['passage', 'query', 'entities', 'entity_spans', 'answers'],
+		"label": None,
+		"splits": ['train', 'validation', 'test'],
+		"splits_num_examples": [100730, 10000, 10000],
 	},
 
-	"rte":{
-		"load_dataset_args":('glue', 'rte'),
-		"sentence_keys":['sentence1', 'sentence2'],
-		"label":ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"rte": {
+		"load_dataset_args": ('glue', 'rte'),
+		"sentence_keys": ['sentence1', 'sentence2'],
+		"label": ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [3000, 2490, 277],
 	},
 
-	"wic":{
-		"load_dataset_args":('super_glue', 'wic'),
-		"sentence_keys":['word', 'sentence1', 'sentence2', 'start1', 'start2', 'end1', 'end2'],
-		"label":ClassLabel(num_classes=2, names=('False', 'True'), id=None),
-		"splits":['test', 'train', 'validation'],
+	"wic": {
+		"load_dataset_args": ('super_glue', 'wic'),
+		"sentence_keys": ['word', 'sentence1', 'sentence2', 'start1', 'start2', 'end1', 'end2'],
+		"label": ClassLabel(num_classes=2, names=('False', 'True'), id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [1400, 5428, 638],
 	},
 
-	"wsc":{
-		"load_dataset_args":('super_glue', 'wsc'),
-		"sentence_keys":['text', 'span1_index', 'span2_index', 'span1_text', 'span2_text'],
-		"label":ClassLabel(num_classes=2, names=('False', 'True'), id=None),
-		"splits":['test', 'train', 'validation'],
+	"wsc": {
+		"load_dataset_args": ('super_glue', 'wsc'),
+		"sentence_keys": ['text', 'span1_index', 'span2_index', 'span1_text', 'span2_text'],
+		"label": ClassLabel(num_classes=2, names=('False', 'True'), id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [146, 554, 104],
 	},
 
-	"wsc.fixed":{
-		"load_dataset_args":('super_glue', 'wsc.fixed'),
-		"sentence_keys":['text', 'span1_index', 'span2_index', 'span1_text', 'span2_text'],
-		"label":ClassLabel(num_classes=2, names=('False', 'True'), id=None),
-		"splits":['test', 'train', 'validation'],
+	"wsc.fixed": {
+		"load_dataset_args": ('super_glue', 'wsc.fixed'),
+		"sentence_keys": ['text', 'span1_index', 'span2_index', 'span1_text', 'span2_text'],
+		"label": ClassLabel(num_classes=2, names=('False', 'True'), id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [146, 554, 104],
 	},
 
-	"axb":{
-		"load_dataset_args":('super_glue', 'axb'),
-		"sentence_keys":['sentence1', 'sentence2'],
-		"label":ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
-		"splits":['test'],
+	"axb": {
+		"load_dataset_args": ('super_glue', 'axb'),
+		"sentence_keys": ['sentence1', 'sentence2'],
+		"label": ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
+		"splits": ['test'],
+		"splits_num_examples": [1104],
 	},
 
-	"axg":{
-		"load_dataset_args":('super_glue', 'axg'),
-		"sentence_keys":['premise', 'hypothesis'],
-		"label":ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
-		"splits":['test'],
+	"axg": {
+		"load_dataset_args": ('super_glue', 'axg'),
+		"sentence_keys": ['premise', 'hypothesis'],
+		"label": ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
+		"splits": ['test'],
+		"splits_num_examples": [356],
 	},
 
-	"cola":{
-		"load_dataset_args":('glue', 'cola'),
-		"sentence_keys":['sentence'],
-		"label":ClassLabel(num_classes=2, names=['unacceptable', 'acceptable'], id=None),
-		"splits":['train', 'validation', 'test'],
+	"cola": {
+		"load_dataset_args": ('glue', 'cola'),
+		"sentence_keys": ['sentence'],
+		"label": ClassLabel(num_classes=2, names=['unacceptable', 'acceptable'], id=None),
+		"splits": ['train', 'validation', 'test'],
+		"splits_num_examples": [8551, 1043, 1063],
 	},
 
-	"sst2":{
-		"load_dataset_args":('glue', 'sst2'),
-		"sentence_keys":['sentence'],
-		"label":ClassLabel(num_classes=2, names=['negative', 'positive'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"sst2": {
+		"load_dataset_args": ('glue', 'sst2'),
+		"sentence_keys": ['sentence'],
+		"label": ClassLabel(num_classes=2, names=['negative', 'positive'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [1821, 67349, 872],
 	},
 
-	"mrpc":{
-		"load_dataset_args":('glue', 'mrpc'),
-		"sentence_keys":['sentence1', 'sentence2'],
-		"label":ClassLabel(num_classes=2, names=['not_equivalent', 'equivalent'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"mrpc": {
+		"load_dataset_args": ('glue', 'mrpc'),
+		"sentence_keys": ['sentence1', 'sentence2'],
+		"label": ClassLabel(num_classes=2, names=['not_equivalent', 'equivalent'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [1725, 3668, 408],
 	},
 
-	"qqp":{
-		"load_dataset_args":('glue', 'qqp'),
-		"sentence_keys":['question1', 'question2'],
-		"label":ClassLabel(num_classes=2, names=['not_duplicate', 'duplicate'], id=None),
-		"splits":['train', 'validation', 'test'],
+	"qqp": {
+		"load_dataset_args": ('glue', 'qqp'),
+		"sentence_keys": ['question1', 'question2'],
+		"label": ClassLabel(num_classes=2, names=['not_duplicate', 'duplicate'], id=None),
+		"splits": ['train', 'validation', 'test'],
+		"splits_num_examples": [363846, 40430, 390965],
 	},
 
-	"stsb":{
-		"load_dataset_args":('glue', 'stsb'),
-		"sentence_keys":['sentence1', 'sentence2'],
-		"label":Value(dtype='float32', id=None),
-		"splits":['test', 'train', 'validation'],
+	"stsb": {
+		"load_dataset_args": ('glue', 'stsb'),
+		"sentence_keys": ['sentence1', 'sentence2'],
+		"label": Value(dtype='float32', id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [1379, 5749, 1500],
 	},
 
-	"mnli":{
-		"load_dataset_args":('glue', 'mnli'),
-		"sentence_keys":['premise', 'hypothesis'],
-		"label":ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
-		"splits":['test_matched', 'test_mismatched', 'train', 'validation_matched', 'validation_mismatched'],
+	"mnli": {
+		"load_dataset_args": ('glue', 'mnli'),
+		"sentence_keys": ['premise', 'hypothesis'],
+		"label": ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
+		"splits": ['test_matched', 'test_mismatched', 'train', 'validation_matched', 'validation_mismatched'],
+		"splits_num_examples": [9796, 9847, 392702, 9815, 9832],
 	},
 
-	"mnli_mismatched":{
-		"load_dataset_args":('glue', 'mnli_mismatched'),
-		"sentence_keys":['premise', 'hypothesis'],
-		"label":ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
-		"splits":['test', 'validation'],
+	"mnli_mismatched": {
+		"load_dataset_args": ('glue', 'mnli_mismatched'),
+		"sentence_keys": ['premise', 'hypothesis'],
+		"label": ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
+		"splits": ['test', 'validation'],
+		"splits_num_examples": [9847, 9832],
 	},
 
-	"mnli_matched":{
-		"load_dataset_args":('glue', 'mnli_matched'),
-		"sentence_keys":['premise', 'hypothesis'],
-		"label":ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
-		"splits":['test', 'validation'],
+	"mnli_matched": {
+		"load_dataset_args": ('glue', 'mnli_matched'),
+		"sentence_keys": ['premise', 'hypothesis'],
+		"label": ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
+		"splits": ['test', 'validation'],
+		"splits_num_examples": [9796, 9815],
 	},
 
-	"qnli":{
-		"load_dataset_args":('glue', 'qnli'),
-		"sentence_keys":['question', 'sentence'],
-		"label":ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"qnli": {
+		"load_dataset_args": ('glue', 'qnli'),
+		"sentence_keys": ['question', 'sentence'],
+		"label": ClassLabel(num_classes=2, names=['entailment', 'not_entailment'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [5463, 104743, 5463],
 	},
 
-	"wnli":{
-		"load_dataset_args":('glue', 'wnli'),
-		"sentence_keys":['sentence1', 'sentence2'],
-		"label":ClassLabel(num_classes=2, names=['not_entailment', 'entailment'], id=None),
-		"splits":['test', 'train', 'validation'],
+	"wnli": {
+		"load_dataset_args": ('glue', 'wnli'),
+		"sentence_keys": ['sentence1', 'sentence2'],
+		"label": ClassLabel(num_classes=2, names=['not_entailment', 'entailment'], id=None),
+		"splits": ['test', 'train', 'validation'],
+		"splits_num_examples": [146, 635, 71],
 	},
 
-	"ax":{
-		"load_dataset_args":('glue', 'ax'),
-		"sentence_keys":['premise', 'hypothesis'],
-		"label":ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
-		"splits":['test'],
+	"ax": {
+		"load_dataset_args": ('glue', 'ax'),
+		"sentence_keys": ['premise', 'hypothesis'],
+		"label": ClassLabel(num_classes=3, names=['entailment', 'neutral', 'contradiction'], id=None),
+		"splits": ['test'],
+		"splits_num_examples": [1104],
 	},
 
-	"imdb":{
-		"load_dataset_args":('imdb', 'plain_text'),
-		"sentence_keys":['text'],
-		"label":ClassLabel(num_classes=2, names=['neg', 'pos'], id=None),
-		"splits":['train', 'test', 'unsupervised'],
+	"imdb": {
+		"load_dataset_args": ('imdb', 'plain_text'),
+		"sentence_keys": ['text'],
+		"label": ClassLabel(num_classes=2, names=['neg', 'pos'], id=None),
+		"splits": ['train', 'test', 'unsupervised'],
+		"splits_num_examples": [25000, 25000, 50000],
 	},
 
-	"hans":{
-		"load_dataset_args":('hans', 'plain_text'),
-		"sentence_keys":['premise', 'hypothesis', 'parse_premise', 'parse_hypothesis', 'binary_parse_premise', 'binary_parse_hypothesis', 'heuristic', 'subcase', 'template'],
-		"label":ClassLabel(num_classes=2, names=['entailment', 'non-entailment'], id=None),
-		"splits":['train', 'validation'],
+	"hans": {
+		"load_dataset_args": ('hans', 'plain_text'),
+		"sentence_keys": ['premise', 'hypothesis', 'parse_premise', 'parse_hypothesis', 'binary_parse_premise', 'binary_parse_hypothesis', 'heuristic', 'subcase', 'template'],
+		"label": ClassLabel(num_classes=2, names=['entailment', 'non-entailment'], id=None),
+		"splits": ['train', 'validation'],
+		"splits_num_examples": [30000, 30000],
+	},
+
+	"squad": {
+		"load_dataset_args": ('squad', 'plain_text'),
+		"sentence_keys": ['title', 'context', 'question', 'answers'],
+		"label": None,
+		"splits": ['train', 'validation'],
+		"splits_num_examples": [87599, 10570],
+	},
+
+	"squad_v2": {
+		"load_dataset_args": ('squad_v2', 'squad_v2'),
+		"sentence_keys": ['title', 'context', 'question', 'answers'],
+		"label": None,
+		"splits": ['train', 'validation'],
+		"splits_num_examples": [130319, 11873],
 	},
 }
--- src/scripts/build_datasets_info.py
+++ src/scripts/build_datasets_info.py
@@ -1,8 +1,48 @@
+## Running this script generates the lib/hf_datasets_info.py file
+
 import json
 import os 
 import datasets
+import sys
+from typing import Union
+import logging
 from datasets import Value
-# Extracts relevant info for training/validation from DatasetInfo object
+
+sys.path.insert(0, "..") 
+
+## This scripts creates and updates the ../lib/hf_datasets_info.py file
+## Usages: 
+# `python build_datasets_info.py`
+# `python build_datasets_info.py task1 task2`
+
+## Generated file contains the following:
+# valid_task_names: as a list of valid finetuning task names that can be processed by the code
+# dataset_info: a dictionary mapping finetuning tasks to the relevant subset of info from DatasetInfo object. 
+# For each task/dataset, the dataset_info stores: args for load_dataset(), names of splits, label information, and sentence keys for the input features. 
+## Ref: Super GLUE tasks: ['boolq', 'cb', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc', 'wsc.fixed', 'axb', 'axg']
+## Ref: GLUE taks: ['cola', 'sst2', 'mrpc', 'qqp', 'stsb', 'mnli', 'mnli_mismatched', 'mnli_matched', 'qnli', 'rte', 'wnli', 'ax']
+
+default_builders = ["super_glue", "glue", "imdb", "hans", "squad", "squad_v2"]
+
+def create_datasets_info_dict(builder_names):
+
+    datasets_info = {}
+
+    for builder in builder_names:
+        info = datasets.get_dataset_infos(builder)
+        tasks = list(info.keys())
+        # If the builder has only one task, use the builder_name as the key in datasets_info, else use the task name (config_name) as the key
+        if len(tasks)==1: 
+            TaskInfo = info[tasks[0]]
+            datasets_info[builder] = get_task_info_dict(TaskInfo)
+        else:
+            for task, TaskInfo in info.items():
+                datasets_info[task] = get_task_info_dict(TaskInfo)
+
+    return datasets_info
+
+
+# Extracts relevant info for train/val from a single DatasetInfo object
 def get_task_info_dict(TaskInfo: datasets.DatasetInfo) -> dict:
     features = TaskInfo.features
     if "label" in features.keys():
@@ -14,65 +54,59 @@ def get_task_info_dict(TaskInfo: datasets.DatasetInfo) -> dict:
 
     if "idx" in features.keys():
         features.pop("idx")
+    if "id" in features.keys():
+        features.pop("id")
     sentence_keys = list(features.keys())
+
+    splits = list(TaskInfo.splits.keys())
+    splits_size = []
+    for split in splits:
+        splits_size.append(TaskInfo.splits[split].num_examples)
 
     task_info = dict(
         load_dataset_args=(TaskInfo.builder_name,TaskInfo.config_name),
         sentence_keys=sentence_keys,
         label=label,
-        splits=list(TaskInfo.splits.keys())
+        splits=splits,
+        splits_num_examples=splits_size
     )
 
     return task_info
 
-## Add new datasets here.
-## 'datsets_info' is a dictionary of finetuning datasets with a relevant subset of info from DatasetInfo object. 
-# For each task/dataset, the dataset_info stores: args for load_dataset(), names of splits, label information, and sentence keys for the input features. 
 
-def create_datasets_info_dict():
+def dump_datasets_info(datasets_info, overwrite = True):
 
-    datasets_info = {}
+    file_path = os.path.join("..", "lib", "hf_datasets_info.py") 
+    if overwrite or not(os.path.exists(file_path)):
+        logging.info(f"Creating/overwriting {file_path}")
+    else: 
+        from lib.hf_datasets_info import datasets_info as old_datasets_info
+        datasets_info = {**old_datasets_info, **datasets_info}
+        logging.info(f"Updating dataset_info in existing {file_path}")
 
-    ## First we add tasks from data collections "super_glue" and "glue"
-    ## Super GLUE tasks
-    # ['boolq', 'cb', 'copa', 'multirc', 'record', 'rte', 'wic', 'wsc', 'wsc.fixed', 'axb', 'axg']
-    ## GLUE taks
-    # ['cola', 'sst2', 'mrpc', 'qqp', 'stsb', 'mnli', 'mnli_mismatched', 'mnli_matched', 'qnli', 'rte', 'wnli', 'ax']    
-    datacollection_tasks = {
-        "super_glue": datasets.get_dataset_config_names("super_glue"),
-        "glue": datasets.get_dataset_config_names("glue")
-    }
-    datacollection_info = {
-        "super_glue": datasets.get_dataset_infos("super_glue"),
-        "glue": datasets.get_dataset_infos("glue")
-    }
-
-    for builder, info in datacollection_info.items():
-        for task, TaskInfo in info.items():
-            datasets_info[task] = get_task_info_dict(TaskInfo)
-
-    ## Now we add individual datasets/IMDB and HANS datasets
-    for ds in ("imdb","hans"):
-        
-        TaskInfo = datasets.get_dataset_infos(ds)['plain_text']
-        datasets_info[ds] =  get_task_info_dict(TaskInfo)
-
-    return datasets_info
-
-def dump_datasets_info(datasets_info):
-    with open(os.path.join("..", "lib", "hf_datasets_info.py"), 'w') as finfo:
+    with open(file_path, 'w') as finfo:
         finfo.write('# Automatically generated using ../scripts/build_datasets_info.py. \n\n')
         finfo.write('from datasets import ClassLabel, Value\n\n')
         finfo.write(f'valid_task_names={list(datasets_info.keys())}\n\n')
         finfo.write('datasets_info = {\n')
         for key, value in datasets_info.items():
-            finfo.write(f'\n\t"{key}":{{\n')
+            finfo.write(f'\n\t"{key}": {{\n')
             for subkey,subvalue in datasets_info[key].items():
-                finfo.write(f'\t\t"{subkey}":{subvalue},\n')
+                finfo.write(f'\t\t"{subkey}": {subvalue},\n')
             finfo.write('\t},\n')
         finfo.write('}\n')
 
+def main(add_tasks=None):
+    if add_tasks:
+        builder_names = add_tasks
+        overwrite = False
+    else:
+        overwrite = True
+        builder_names = default_builders
+
+    dataset_info = create_datasets_info_dict(builder_names)
+    dump_datasets_info (dataset_info, overwrite)
 
 if __name__== "__main__":
-    datasets_info = create_datasets_info_dict()
-    dump_datasets_info(datasets_info)
+    add_tasks = sys.argv[1:]
+    main(add_tasks)
